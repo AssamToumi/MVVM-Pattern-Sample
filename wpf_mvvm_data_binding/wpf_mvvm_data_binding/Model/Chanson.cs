bc89c4781c5968826cfbca9b70e8501ea86475bd
Assam Toumi <assamtomi@outlook.com>
2020-07-06 20:28:23 +0100

    done!

---
+++ wpf_mvvm_data_binding/wpf_mvvm_data_binding/Model/Chanson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_mvvm_data_binding.Models
+{
+    public class Chanson
+    {
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+    }
+}
---
+++ wpf_mvvm_data_binding/wpf_mvvm_data_binding/ViewModel/ChansonViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf_mvvm_data_binding.Models;
+
+namespace wpf_mvvm_data_binding.ViewModel
+{
+    class ChansonViewModel : INotifyPropertyChanged
+    {
+        private Chanson _model;
+
+        public ChansonViewModel()
+        {
+            _model = new Chanson { Name = "Song", Gender = "PoP" };
+        }
+
+        public string ChansonName
+        {
+            get { return _model.Name; }
+            set
+            {
+                _model.Name = value;
+                OnPropertyChanged("ChansonName");
+            }
+        }
+
+        public string ChansonGender
+        {
+            get { return _model.Gender; }
+            set
+            {
+                _model.Gender = value;
+                OnPropertyChanged("ChansonGender");
+            }
+        }
+
+        //Event binds view to ViewModel.
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                this.PropertyChanged(this, e);
+            }
+        }
+
+    }
+}
